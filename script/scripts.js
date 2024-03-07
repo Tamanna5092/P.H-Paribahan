639de7ba4122e4f324968d6b969e59ec0657be2e
Tamanna5092 <tamannaaktermoni51@gmail.com>
2024-03-07 18:45:09 +0600

    seat count

--- script/scripts.js
+++ script/scripts.js
@@ -1,1 +1,10 @@
-console.log('connected')
+const allBtn = document.getElementsByClassName('add-btn');
+
+let count = 0;
+for(const btn of allBtn){
+    btn.addEventListener('click',function(event){
+        count = count + 1;
+        console.log(count)
+        document.getElementById('seat-count').innerText = count;
+    });
+}
