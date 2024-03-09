44f69f25b167965c20fbf012efa2e67b1a6f290a
Tamanna5092 <tamannaaktermoni51@gmail.com>
2024-03-09 17:09:20 +0600

    add success screen

--- script/scripts.js
+++ script/scripts.js
@@ -26,22 +26,47 @@ for(const btn of allBtn){
         economyClass.appendChild(li2);
         price.appendChild(li3)
 
-
-
-        const totalCost = document.getElementById('total-cost').innerText;
-        const convertedTotalCost = parseInt(totalCost);
-        const sum = convertedTotalCost + parseInt(li3.innerText);
-
-        const grandTotalCost = document.getElementById('grand-total').innerText;
-        const convertedGrandTotalCost = parseInt(grandTotalCost);
-        const sum2 = convertedGrandTotalCost + sum;
-
-        setInnerText('total-cost',sum);
-        setInnerText('grand-total',sum2);
+ 
+        // const element = document.getElementsByClassName('add-btn');
+        // element.classList.add('bg-green-500');
+        // setBackgroundColorByClassName(element)
+  
+        totalCost('total-cost', li3.innerText);
+        grandTotalCost('grand-total', li3.innerText);
+        
         setInnerText('seat-count',count);
     });
 }
 
 function setInnerText(id,value){
     document.getElementById(id).innerText = value;
+}
+
+function totalCost(id,value){
+    const totalCost = document.getElementById(id).innerText;
+    const convertedTotalCost = parseInt(totalCost);
+    const sum = convertedTotalCost + parseInt(value);
+    setInnerText('total-cost',sum);
+}
+
+function grandTotalCost(category){
+    const totalCost = document.getElementById('total-cost').innerText;
+    const convertedTotalCost = parseInt(totalCost);
+    
+    if(category == 'NEW15'){
+        setInnerText('grand-total',convertedTotalCost - (totalCost * 15 / 100));
+    }
+    else if(category == 'Couple20'){
+        setInnerText('grand-total',convertedTotalCost - (totalCost * 20 / 100));
+    }
+    else{
+        setInnerText('grand-total',convertedTotalCost);
+    }
+}
+
+function nextButton(){
+    const ticketSectionContainer = document.getElementById('ticker-section-container');
+    ticketSectionContainer.classList.add('hidden');
+    const bookingSection = document.getElementById('booking-section');
+    bookingSection.classList.remove('hidden');
 }
